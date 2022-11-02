59fb7b486ed95e4feb80c378fe7e9e818d509cdc
Andrew <niramrus@gmail.com>
2022-11-02 13:23:52 +0500

    ex214

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+    private Deth8 death;
+    @Autowired
+    public Egg6(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + death.toString();
     }
 }
